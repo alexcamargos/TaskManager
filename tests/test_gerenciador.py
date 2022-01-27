a1b3ca38dd8504ca8a1a5310294412378c4dc5a7
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-01-27 12:31:46 -0300

    add: test for GET /tarefas response is application/json.

--- tests/test_gerenciador.py
+++ tests/test_gerenciador.py
@@ -9,3 +9,9 @@ def test_quando_listar_tarefas_devo_ter_como_retorno_codigo_de_status_200():
     response = client.get('/tarefas')
 
     assert response.status_code == status.HTTP_200_OK
+
+def test_quando_listar_tarefas_formato_de_retorno_deve_ser_json():
+    client = TestClient(app)
+    response = client.get('/tarefas')
+
+    assert response.headers['Content-Type'] == 'application/json'
