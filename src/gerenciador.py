85709906de2d300195416d1b0ab7cd6678c2b91d
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-01-28 06:35:36 -0300

    add: test for a complete task structure.

--- src/gerenciador.py
+++ src/gerenciador.py
@@ -2,7 +2,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+TAREFAS = [
+    {
+        "id": "1",
+        "titulo": "Fazer compras no supermercado",
+        "descrição": "Lista de compras: leite, pão e ovos.",
+        "estado": "não finalizado",
+    },
+    {
+        "id": "2",
+        "titulo": "Levar o cachorro para tosar",
+        "descrição": "Como estamos o verão e o pet está muito peludo, faz-se necessário.",
+        "estado": "não finalizado",
+    },
+    {
+        "id": "3",
+        "titulo": "Colocar as roupas na máquina para lavar",
+        "descrição": "Como só tenho a roupa que estou usando é urgente lavar as sujas.",
+        "estado": "não finalizado",
+    },
+]
+
 
 @app.get('/tarefas')
 def listar():
-    return ''
+    return TAREFAS
--- tests/test_gerenciador.py
+++ tests/test_gerenciador.py
@@ -1,7 +1,9 @@
+from http import client
+from urllib import response
 from fastapi.testclient import TestClient
 from fastapi import status
 
-from src.gerenciador import app
+from src.gerenciador import TAREFAS, app
 
 
 def test_quando_listar_tarefas_devo_ter_como_retorno_codigo_de_status_200():
@@ -15,3 +17,45 @@ def test_quando_listar_tarefas_formato_de_retorno_deve_ser_json():
     response = client.get('/tarefas')
 
     assert response.headers['Content-Type'] == 'application/json'
+
+def test_quando_listar_tarefas_retorno_deve_ser_uma_lista():
+    client = TestClient(app)
+    response = client.get('/tarefas')
+
+    assert isinstance(response.json(), list)
+
+def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_ID():
+    TAREFAS.append({'id':1})
+    client = TestClient(app)
+    response = client.get('/tarefas')
+
+    assert 'id' in response.json().pop()
+
+    TAREFAS.clear()
+
+def test_quanto_listar_tarefas_a_tarefa_deve_possuir_titulo():
+    TAREFAS.append({'titulo':'Exemplo de Título'})
+    client = TestClient(app)
+    response = client.get('/tarefas')
+
+    assert 'titulo' in response.json().pop()
+
+    TAREFAS.clear()
+
+def test_quando_listar_tarefas_a_tarefa_deve_possuir_descrição():
+    TAREFAS.append({'descrição':'Exemplo de descrição'})
+    client = TestClient(app)
+    response = client.get('/tarefas')
+
+    assert 'descrição' in response.json().pop()
+
+    TAREFAS.clear()
+
+def test_quando_listar_tarefas_a_tarefa_deve_possuir_estado():
+    TAREFAS.append({'estado':'Exemplo de estado'})
+    client = TestClient(app)
+    response = client.get('/tarefas')
+
+    assert 'estado' in response.json().pop()
+
+    TAREFAS.clear()
