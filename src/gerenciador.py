c42d926372d839964934a7b0cc1a8596b7552639
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-01-29 15:27:51 -0300

    add: adicionado funcionalidade de criar tarefas.

--- src/gerenciador.py
+++ src/gerenciador.py
@@ -1,31 +1,52 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, constr
+from uuid import UUID, uuid4
+from enum import Enum
 
-app = FastAPI()
 
-TAREFAS = [
-    {
-        "id": "1",
-        "titulo": "Fazer compras no supermercado",
-        "descrição": "Lista de compras: leite, pão e ovos.",
-        "estado": "não finalizado",
-    },
-    {
-        "id": "2",
-        "titulo": "Levar o cachorro para tosar",
-        "descrição":
-        "Como estamos o verão e o pet está muito peludo, faz-se necessário.",
-        "estado": "não finalizado",
-    },
-    {
-        "id": "3",
-        "titulo": "Colocar as roupas na máquina para lavar",
-        "descrição":
-        "Como só tenho a roupa que estou usando é urgente lavar as sujas.",
-        "estado": "não finalizado",
-    },
+class EstadosPossiveis(str, Enum):
+    to_do = 'não finalizado'
+    done = 'finalizado'
+
+
+class InputTask(BaseModel):
+    title: constr(min_length=3, max_length=50)
+    description: constr(max_length=200)
+    state: EstadosPossiveis = EstadosPossiveis.to_do
+
+
+class Task(InputTask):
+    id: UUID
+
+
+TASKS = [
+    Task(id=uuid4(),
+         title='Fazer compras no supermercado',
+         description='Lista de compras: leite, pão e ovos.',
+         state=EstadosPossiveis.done),
+    Task(id=uuid4(),
+         title='Levar o cachorro ao veterinário',
+         description='Consulta anual de rotina'),
+    Task(id=uuid4(),
+         title='Colocar as roupas na máquina para lavar',
+         description=
+         'Como só tenho a roupa que estou usando é urgente lavar as sujas.')
 ]
 
+task_manager_app = FastAPI()
 
-@app.get('/tarefas')
-def listar():
-    return TAREFAS
+
+@task_manager_app.get('/task')
+def get_tasks():
+    return TASKS
+
+
+@task_manager_app.post('/task', response_model=Task, status_code=201)
+def post_task(tarefa: InputTask):
+
+    new_task = tarefa.dict()
+    new_task.update({'id': uuid4()})
+
+    TASKS.append(new_task)
+
+    return new_task
--- tests/test_gerenciador.py
+++ tests/test_gerenciador.py
@@ -1,67 +1,177 @@
-from http import client
-from urllib import response
 from fastapi.testclient import TestClient
 from fastapi import status
 
-from src.gerenciador import TAREFAS, app
+from src.gerenciador import TASKS, Task, task_manager_app
 
 
 def test_quando_listar_tarefas_devo_ter_como_retorno_codigo_de_status_200():
-    client = TestClient(app)
-    response = client.get('/tarefas')
+    client = TestClient(task_manager_app)
 
+    response = client.get('/task')
     assert response.status_code == status.HTTP_200_OK
 
 
 def test_quando_listar_tarefas_formato_de_retorno_deve_ser_json():
-    client = TestClient(app)
-    response = client.get('/tarefas')
+    client = TestClient(task_manager_app)
 
+    response = client.get('/task')
     assert response.headers['Content-Type'] == 'application/json'
 
 
 def test_quando_listar_tarefas_retorno_deve_ser_uma_lista():
-    client = TestClient(app)
-    response = client.get('/tarefas')
+    client = TestClient(task_manager_app)
 
+    response = client.get('/task')
     assert isinstance(response.json(), list)
 
 
 def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_ID():
-    TAREFAS.append({'id': 1})
-    client = TestClient(app)
-    response = client.get('/tarefas')
+    client = TestClient(task_manager_app)
+    TASKS.append({'id': 1})
 
+    response = client.get('/task')
     assert 'id' in response.json().pop()
 
-    TAREFAS.clear()
+    TASKS.clear()
 
 
 def test_quanto_listar_tarefas_a_tarefa_deve_possuir_titulo():
-    TAREFAS.append({'titulo': 'Exemplo de Título'})
-    client = TestClient(app)
-    response = client.get('/tarefas')
+    client = TestClient(task_manager_app)
+    TASKS.append({'title': 'Exemplo de Título'})
 
-    assert 'titulo' in response.json().pop()
+    response = client.get('/task')
+    assert 'title' in response.json().pop()
 
-    TAREFAS.clear()
+    TASKS.clear()
 
 
 def test_quando_listar_tarefas_a_tarefa_deve_possuir_descrição():
-    TAREFAS.append({'descrição': 'Exemplo de descrição'})
-    client = TestClient(app)
-    response = client.get('/tarefas')
+    client = TestClient(task_manager_app)
+    TASKS.append({"description": 'Exemplo de descrição'})
 
-    assert 'descrição' in response.json().pop()
+    response = client.get('/task')
+    assert "description" in response.json().pop()
 
-    TAREFAS.clear()
+    TASKS.clear()
 
 
 def test_quando_listar_tarefas_a_tarefa_deve_possuir_estado():
-    TAREFAS.append({'estado': 'Exemplo de estado'})
-    client = TestClient(app)
-    response = client.get('/tarefas')
+    TASKS.append({'state': 'Exemplo de estado'})
+    client = TestClient(task_manager_app)
 
-    assert 'estado' in response.json().pop()
+    response = client.get('/task')
+    assert 'state' in response.json().pop()
 
-    TAREFAS.clear()
+    TASKS.clear()
+
+
+def test_recurso_tarefas_deve_aceitar_o_verbo_POST():
+    client = TestClient(task_manager_app)
+
+    response = client.post('/task')
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_quando_uma_tarefa_e_submetida_deve_possuir_um_titulo():
+    client = TestClient(task_manager_app)
+
+    response = client.post('/task', json={})
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_titulo_da_tarefa_deve_conter_entre_3_e_50_caracteres():
+    client = TestClient(task_manager_app)
+
+    response = client.post('/task', json={'title': '*' * 2})
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+    response = client.post('/task', json={'title': '*' * 51})
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_quando_uma_tarefa_e_submetida_deve_possuir_uma_descricao():
+    client = TestClient(task_manager_app)
+
+    response = client.post('/task', json={'title': 'Título'})
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_descrição_da_tarefa_pode_conter_no_maximo_200_caracteres():
+    client = TestClient(task_manager_app)
+
+    response = client.post('/task',
+                           json={
+                               'title': 'Título',
+                               "description": '*' * 201
+                           })
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_quando_criar_uma_tarefa_a_mesma_deve_ser_retornada():
+    client = TestClient(task_manager_app)
+    task = {
+        'title': 'Título de exemplo',
+        "description": "Descrição de exemplo.",
+        'state': 'não finalizado'
+    }
+
+    response = client.post('/task', json=task)
+    assert response.json()['title'] == task['title'] and \
+           response.json()['description'] == task['description'] and \
+           response.json()['state'] == task['state']
+
+
+def test_quando_criar_uma_tarefa_seu_id_deve_ser_unico():
+    client = TestClient(task_manager_app)
+    task_1 = {
+        'title': 'Exemplo de Título',
+        "description": 'Exemplo de descrição',
+    }
+    task_2 = {
+        'title': 'Exemplo de Título',
+        "description": 'Exemplo de descrição',
+    }
+
+    response_1 = client.post('/task', json=task_1)
+    response_2 = client.post('/task', json=task_2)
+
+    assert response_1.json()['id'] != response_2.json()['id']
+
+
+def test_quando_criar_uma_tarefa_seu_estado_padrao_deve_ser_nao_finalizado():
+    client = TestClient(task_manager_app)
+    task = {
+        'title': 'Exemplo de Título',
+        "description": 'Exemplo de descrição'
+    }
+
+    response = client.post('/task', json=task)
+    assert response.json()['state'] == 'não finalizado'
+
+
+def test_quando_criar_uma_tarefa_codigo_de_status_retornado_deve_ser_201():
+    client = TestClient(task_manager_app)
+    task = {
+        'title': 'Exemplo de Título',
+        "description": 'Exemplo de descrição.'
+    }
+
+    response = client.post('/task', json=task)
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+def test_quando_criar_uma_nova_tarefa_a_mesma_deve_ser_persistida():
+    client = TestClient(task_manager_app)
+
+    task = {
+        'title': 'Exemplo de Título',
+        'description': 'Exemplo de descrição'
+    }
+    response = client.post('/task', json=task)
+
+    print(response.json())
+
+    assert response.status_code == status.HTTP_201_CREATED and len(TASKS) == 6
+
+    TASKS.clear()
